e1484a14590e7f07c5f826e89e251c0d4a64a324
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-10-12 14:29:50 +0300

    update JDK and checkstyle plugin versions
    remove encoding config from checkstyle plugin
    
    first try
    
    gix bugs
    
    rewrite
    
    fixed bugs
    
    fix bugs 2.0
    
    Update GitHub Actions versions
    
    Update ci.yml
    
    remake homework
    
    done
    
    done
    
    fix

---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,17 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    private static final int BOUNDARY_NUMBER = 100;
+    public Ball getRandomBall() {
+        Random random = new Random();
+        int indexColor = random.nextInt(Color.values().length);
+        int randomto100 = random.nextInt(BOUNDARY_NUMBER);
+        Color color = Color.values()[indexColor];
+        Ball ball = new Ball();
+        ball.setNumber(randomto100);
+        ball.setColor(color);
+        return ball;
+    }
+}
